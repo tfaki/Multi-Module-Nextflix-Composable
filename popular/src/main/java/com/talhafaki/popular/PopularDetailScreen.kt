6ae26162e00024d413fdc6caef24aabc543b4047
talhafaki <talhafaki05@gmail.com>
2022-03-17 09:34:14 +0300

    create detail screens and contents func

---
+++ popular/src/main/java/com/talhafaki/popular/PopularDetailScreen.kt
@@ -0,0 +1,20 @@
+package com.talhafaki.popular
+
+import androidx.compose.runtime.Composable
+import com.talhafaki.common.theme.NextflixComposableTheme
+
+/**
+ * Created by talhafaki on 16.03.2022.
+ */
+
+@Composable
+fun PopularDetailScreen() {
+    NextflixComposableTheme(darkTheme = true) {
+        PopularDetailContent()
+    }
+}
+
+@Composable
+fun PopularDetailContent() {
+    /* do something */
+}
---
+++ upcoming/src/main/java/com/talhafaki/upcoming/UpcomingDetailScreen.kt
@@ -0,0 +1,20 @@
+package com.talhafaki.upcoming
+
+import androidx.compose.runtime.Composable
+import com.talhafaki.common.theme.NextflixComposableTheme
+
+/**
+ * Created by talhafaki on 16.03.2022.
+ */
+
+@Composable
+fun UpcomingDetailScreen() {
+    NextflixComposableTheme(darkTheme = true) {
+        UpcomingDetailContent()
+    }
+}
+
+@Composable
+fun UpcomingDetailContent() {
+    /* do something */
+}
